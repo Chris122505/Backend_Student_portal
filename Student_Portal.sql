fa60a3734f474c91d64e71516468b844b3a5d18d
Chris122505 <ccapus45@gmail.com>
2025-11-18 13:00:06 +0800

    Redefine student and related tables in SQL

--- Student_Portal.sql
+++ Student_Portal.sql
@@ -1,33 +1,68 @@
-CREATE TABLE student(
-  student_id    VARCHAR(10)   NOT NULL
- ,last_name     VARCHAR(100)  DEFAULT''
- ,first_name    VARCHAR(100)  DEFAULT''
- ,middle_name   VARCHAR(100)  DEFAULT''
- ,gender        VARCHAR(1)    DEFAULT''
- ,birthday      DATE          NOT NULL
- ,age           INT           DEFAULT 0
- ,nationality   VARCHAR(100)  DEFAULT''
- ,blood_type    VARCHAR(10)   DEFAULT''
- ,address       VARCHAR(500)  DEFAULT''
- ,student_pass  VARCHAR(20)   DEFAULT''
- ,year_level    INT           DEFAULT 0
- ,block_no      INT           DEFAULT 0
- ,status        VARCHAR(1)    DEFAULT'A'
+CREATE TABLE STUDENT(
+  student_id  VARCHAR(10)  NOT NULL
+ ,course_id   VARCHAR(10)  NOT NULL
+ ,block_id    VARCHAR(10)  NOT NULL
+ ,user_id     VARCHAR(10)  NOT NULL
+ ,last_name   VARCHAR(100) NOT NULL 
+ ,first_name  VARCHAR(100) NOT NULL 
+ ,middle_name VARCHAR(100) NOT NULL  
+ ,gender      VARCHAR(6)   DEFAULT''
+ ,birthday    DATE         DEFAULT'12-OCT-9999'
+ ,age         INTEGER      DEFAULT 0
+ ,nationality VARCHAR(100) DEFAULT''
+ ,blood_type  VARCHAR(10)  DEFAULT''
+ ,contact_no  VARCHAR(15)  DEFAULT''
+ ,email       VARCHAR(200) DEFAULT''
+ ,status      VARCHAR(50)  DEFAULT''
  ,CONSTRAINT student_id_pk PRIMARY KEY(student_id) 
+ ,CONSTRAINT course_id_fk_1 FOREIGN KEY(course_id) REFERENCES course(course_id) 
+ ,CONSTRAINT block_id_fk FOREIGN KEY(block_id) REFERENCES block(block_id) 
+ ,CONSTRAINT user_id_fk FOREIGN KEY(user_id) REFERENCES portal_user(user_id)
 );
 
-CREATE TABLE student_email(
-  student_id      VARCHAR(10)   NOT NULL
- ,email_address   VARCHAR(200)  DEFAULT''
- ,CONSTRAINT student_email_pk PRIMARY KEY(student_id, email_address)
+CREATE TABLE STUDENT_ADDRESS(
+  student_id VARCHAR(10)  NOT NULL
+ ,house_no   VARCHAR(300) DEFAULT''
+ ,street     VARCHAR(300) DEFAULT''
+ ,brgy       VARCHAR(300) DEFAULT''
+ ,town       VARCHAR(300) DEFAULT''
+ ,city       VARCHAR(300) DEFAULT''
+ ,zipcode    INTEGER      DEFAULT 0
  ,CONSTRAINT student_id_fk FOREIGN KEY(student_id) REFERENCES student(student_id) 
 );
 
-CREATE TABLE college(
+CREATE TABLE BLOCK(
+  block_id    VARCHAR(10) NOT NULL
+ ,course_id   VARCHAR(10) NOT NULL
+ ,year_level  INTEGER     DEFAULT 0
+ ,block_no    INTEGER     DEFAULT 0 
+ ,status      VARCHAR(1)  DEFAULT'A'
+ ,CONSTRAINT block_id_pk PRIMARY KEY (block_id) 
+ ,CONSTRAINT course_id_fk FOREIGN KEY (course_id) REFERENCES course(course_id) 
+);
+
+CREATE TABLE COURSE(
+  course_id     VARCHAR(10)   NOT NULL
+ ,college_id    VARCHAR(10)   NOT NULL
+ ,course_name   VARCHAR(500)  NOT NULL 
+ ,CONSTRAINT course_id_pk PRIMARY KEY(course_id) 
+ ,CONSTRAINT college_id_fk FOREIGN KEY(college_id) REFERENCES COLLEGE(college_id)
+);
+
+CREATE TABLE COLLEGE(
   college_id    VARCHAR(10)   NOT NULL
- ,college_name  VARCHAR(500)  DEFAULT''
- ,status        VARCHAR(1)    DEFAULT'A'
+ ,college_name  VARCHAR(300)  DEFAULT''
+ ,dean          VARCHAR(300)  DEFAULT''
  ,CONSTRAINT college_id_pk PRIMARY KEY(college_id) 
 );
 
-CREATE TABLE course
+CREATE TABLE PORTAL_USER(
+  user_id     VARCHAR(10)   NOT NULL
+ ,username    VARCHAR(100)  NOT NULL
+ ,password    VARCHAR(10)   NOT NULL
+ ,user_type   VARCHAR(100)  DEFAULT''
+ ,email       VARCHAR(300)  DEFAULT''
+ ,status      VARCHAR(1)    DEFAULT'A'
+ ,last_login  DATE          DEFAULT'12-OCT-9999'
+ ,CONSTRAINT user_id PRIMARY KEY(user_id)
+);
